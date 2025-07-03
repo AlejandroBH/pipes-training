8e4e0d01473786b46cf0b4b51c7d1827f0855cd0
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-07-03 16:34:28 -0400

    some changes

---
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,30 @@
+import {
+  DatePipe,
+  LowerCasePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
+  templateUrl: './basic-page.component.html',
+})
+export default class BasicPageComponent {
+  nameLower = signal('alejandro');
+  nameUpper = signal('ALEJANDRO');
+  fullName = signal('aLeJAnDrO bArRerA');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanup(() => {
+      clearInterval(interval);
+    });
+  });
+}
